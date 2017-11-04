f3b378925925ba068c7cc0b3192ac990b25bb217
unknown <franciscohernandez.22@hotmail.com>
2017-11-03 23:36:57 -0500

    Initial commit

---
+++ Assets/CardScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class CardScript : NetworkBehaviour
+{
+	public Material myMaterial, empty;
+	public GameObject main;
+	public bool canFlip = true;
+
+	[SyncVar]
+	public int coloreame = 0;
+
+	void Update()
+	{		
+
+		if (coloreame == 1) {
+			GetComponent<Renderer> ().material = myMaterial;
+		} else {
+			GetComponent<Renderer> ().material = empty;
+		}
+	}
+
+	// This [Command] code is called on the Client …
+	// … but it is run on the Server!
+	[Command]
+	public void CmdPaint()
+	{
+		if (coloreame == 1) {
+			coloreame = 0;
+		} else {
+			coloreame = 1;
+		}
+
+		/*var card = gameObject;
+		card.GetComponent<Renderer>().material = myMaterial;
+
+		NetworkServer.Spawn (card);*/
+	}
+
+	void OnMouseDown(){
+		Debug.Log (isClient);
+		Debug.Log (isServer);
+
+		if(!canFlip){
+			return;
+		}
+
+		if (main.GetComponent<MainScript> ().jugador == 0 && isClient && !isServer) {//Host
+			//return;
+		}
+
+		if (main.GetComponent<MainScript> ().jugador == 1 && isServer && isClient) {//Client
+			//return;
+		}
+
+		if(main.GetComponent<MainScript> ().canOpen){
+			coloreame = 1;
+			main.GetComponent<MainScript> ().openCard (gameObject.name, myMaterial.name);
+		}
+	}   
+
+}
---
+++ Assets/MainScript.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class MainScript : NetworkBehaviour {
+
+	[SyncVar]
+	public string carta1, carta2;
+	private string sprite1, sprite2;
+	[SyncVar]
+	public bool canOpen;
+	[SyncVar]
+	public int puntosHost, puntosClient;
+	[SyncVar]
+	public int jugador;
+	public UnityEngine.UI.Text turno, ph, pc;
+
+	// Use this for initialization
+	void Start () {
+		canOpen = true;
+		carta1 = "";
+		carta2 = "";
+		sprite1 = "";
+		sprite2 = "";
+		puntosHost = 0;
+		puntosClient = 0;
+		jugador = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (puntosHost + puntosClient == 8) {			
+			if (puntosHost == puntosClient) {
+				turno.text = "Empate!";
+			} else {
+				if (puntosHost > puntosClient) {
+					turno.text = "Gana Host!";
+				} else {
+					turno.text = "Gana Client!";
+				}
+			}
+		} else {
+			if (jugador == 0) {
+				turno.text = "Turno de: Host";
+			} else {
+				turno.text = "Turno de: Client";
+			}
+		}
+		ph.text = "Puntaje Host: " + puntosHost;
+		pc.text = "Puntaje Client: " + puntosClient;
+	}
+
+	public void openCard(string carta, string sprite){
+		if (carta1 == "") {
+			carta1 = carta;
+			sprite1 = sprite;
+		} else {
+			carta2 = carta;
+			sprite2 = sprite;
+			canOpen = false;
+
+			if (sprite1 == sprite2) {
+				if (jugador == 0) {//Host
+					puntosHost++;
+				} else {//Client
+					puntosClient++;
+				}
+				GameObject.Find (carta1).GetComponent<CardScript> ().canFlip = false;
+				GameObject.Find (carta2).GetComponent<CardScript> ().canFlip = false;
+				resetParams ();
+			} else {
+				StartCoroutine (ResetCards());
+			}
+
+		}
+	}
+
+	void resetParams(){
+		canOpen = true;
+		carta1 = "";
+		carta2 = "";
+		sprite1 = "";
+		sprite2 = "";
+		if (jugador == 0) {
+			jugador = 1;
+		} else {
+			jugador = 0;
+		}
+	}
+
+
+	IEnumerator ResetCards(){
+		yield return new WaitForSeconds (1);
+		GameObject.Find (carta1).GetComponent<CardScript> ().coloreame = 0;
+		GameObject.Find (carta2).GetComponent<CardScript> ().coloreame = 0;
+		resetParams ();
+	}
+}
